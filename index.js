a5355805b1a1c249c9f593cfcf857dd6d4cdc343
Aniket-Patidar <aniketpatidar60@gmail.com>
2024-04-14 19:35:02 +0530

    update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+require('dotenv').config();
 const app = express();
 
 // Middleware
